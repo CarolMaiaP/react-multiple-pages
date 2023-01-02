10627a691464c9c282678942e60a9ae8822ad473
Caroline Maia Abreu da Silva <carolmas99@gmail.com>
2023-01-02 16:37:31 -0300

    feat: Link to redirect to home page added

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route  } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, Link  } from 'react-router-dom'
 import './App.css'
 import { Contato } from './component/Contato'
 import { Home } from './component/Home'
@@ -8,6 +8,7 @@ function App() {
   return (
     <div className="App">
       <Router>
+        <Link to="/">Home</Link>
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/contato' element={<Contato />} />
