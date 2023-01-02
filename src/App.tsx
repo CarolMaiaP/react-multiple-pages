fe01d1f554b71405a218e90fbfe8accf6b9d5260
Caroline Maia Abreu da Silva <carolmas99@gmail.com>
2023-01-02 16:34:48 -0300

    feat: home route added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
+import { BrowserRouter as Router, Routes, Route  } from 'react-router-dom'
 import './App.css'
+import { Home } from './component/Home'
 
 function App() {
 
   return (
     <div className="App">
-      
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home />} />
+        </Routes>
+      </Router>
     </div>
   )
 }
