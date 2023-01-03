7045893fb7c650e3669722a4add219e67a10c509
Caroline Maia Abreu da Silva <carolmas99@gmail.com>
2023-01-03 10:39:14 -0300

    feat: profile Link added

--- src/component/Navbar.tsx
+++ src/component/Navbar.tsx
@@ -4,6 +4,7 @@ export function Navbar(){
   return(
     <div className="navbar">
       <Link to="/">Home</Link>
+      <Link to="/profile">Profile</Link>
       <Link to="/contato">Contato</Link>
     </div>
   )
