cbf4d364299b526aa2f34f1b1d0b691b9fd5dd25
Caroline Maia Abreu da Silva <carolmas99@gmail.com>
2023-01-03 10:18:05 -0300

    feat: profile route added

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Contato } from './component/Contato'
 import { Home } from './component/Home'
 import { Navbar } from './component/Navbar'
 import { PageNotFound } from './component/PageNotFound'
+import { Profile } from './component/Profile'
 
 function App() {
 
@@ -14,6 +15,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/contato' element={<Contato />} />
+          <Route path='/profile' element={<Profile />} />
           <Route path='/*' element={<PageNotFound />} />
         </Routes>
       </Router>
